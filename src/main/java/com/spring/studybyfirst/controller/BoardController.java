5cea05b69668520fa17e9752cb336d4e2c525175
HwangSungIn <gommpo111@gmail.com>
2018-04-06 18:19:39 +0900

    스트링 부트 스터디
    6번쨰 커밋
    --
    WORKING
    로깅을 달아주기 위해 설정 작업 우선 Log4J2로 설정 완료
    추후 작업을 위한 acturator 의존성 주입
    ---
    남은 작업 TASK
    
    멀티 디비 세팅은 완료 하였으나 결국 어제의 문제를 해결 못함
    1. 문제의 근원은 DataSource 빈 생성 전 Property들을 읽지 못함 -> 생명 주기가 더 느린듯 함
    해당 원인을 찾지 못해 우선 직접 해당 설정 문자열을 주입 해서 멀티 디비 구축은 완료
    
    2. 로깅 별도 생성 처리 작업 필요

--- src/main/java/com/spring/studybyfirst/controller/BoardController.java
+++ src/main/java/com/spring/studybyfirst/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.spring.studybyfirst.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping(value = "board")
 public class BoardController {
@@ -15,5 +17,19 @@ public class BoardController {
         return "Test";
     }
 
+
+    @GetMapping(value = "/logTest")
+    @ApiOperation(value = "로그테스트용")
+    public String log4jTest(){
+
+        try {
+            log.debug("log debug Test Access {}","HelloWorld");
+            log.info("log info Test Access {}","HelloWorld");
+            throw new Exception();
+        }catch (Exception e){
+            log.error("error region : {}",e.toString());
+        }
+        return "success";
+    }
 
 }
