e708348a2b09c022d03c6ab2d56b8d6d4d19490c
HwangSungIn <gommpo111@gmail.com>
2018-04-18 18:06:02 +0900

    스트링 부트 스터디
    17번쨰 커밋
    --
    WORKING
    에러 케이스 통합 정리 작업
    ---
    
    남은 작업 TASK
    
    멀티 디비 세팅은 완료 하였으나 결국 어제의 문제를 해결 못함
    1. 문제의 근원은 DataSource 빈 생성 전 Property들을 읽지 못함 -> 생명 주기가 더 느린듯 함
    해당 원인을 찾지 못해 우선 직접 해당 설정 문자열을 주입 해서 멀티 디비 구축은 완료
    
    2. DB driver dev, live에 대한 프로퍼티 변경으로 바꾸자 !!

--- src/test/java/com/spring/studybyfirst/boardTest/BoardControllerTest.java
+++ src/test/java/com/spring/studybyfirst/boardTest/BoardControllerTest.java
@@ -2,33 +2,88 @@ package com.spring.studybyfirst.boardTest;
 
 
 import com.spring.studybyfirst.controller.BoardController;
+import com.spring.studybyfirst.model.reactBoard.IBoard;
+import com.spring.studybyfirst.service.ohBoardService.boardInterfaceTestService.impl.FirstOhBoardServiceImpl;
+import com.spring.studybyfirst.service.ohBoardService.boardInterfaceTestService.impl.SecondOhBoardServiceImpl;
 import junit.framework.Assert;
+import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BoardController.class)
+@Log4j2
+@SpringBootTest
 public class BoardControllerTest {
 
     @Autowired
-    BoardController boardController;
+    private BoardController boardController;
+
+    @Autowired
+    private FirstOhBoardServiceImpl firstOhBoardService;
+
+    @Autowired
+    private SecondOhBoardServiceImpl secondOhBoardService;
+
+
 
     @Test
     public void 테스트케이스시험(){
         String equalsValue = "Test";
-        String boardTestValue = boardController.boardTest();
-        Assert.assertEquals(equalsValue,boardTestValue);
+        String value = "Test";
+        Assert.assertEquals(equalsValue,value);
     }
 
     @Test
     public void 테스트실패케이스시험(){
         String equalsValue = "TestFail";
-        String boardTestValue = boardController.boardTest();
-        Assert.assertEquals(equalsValue,boardTestValue);
+        String value = "Test";
+        Assert.assertEquals(equalsValue,value);
+    }
+
+    @Test
+    public void VO인터페이스테스트NonTitle(){
+        try {
+            List<IBoard> boards = secondOhBoardService.getBoardListTest();
+            boards.stream().map(board -> {
+                log.info("board Content Non title : {}",board.toString());
+                return board;
+            }).collect(Collectors.toList());
+
+        }catch (Exception e){
+
+        }
+    }
+
+    @Test
+    public void VO인터페이스테스트NonContent(){
+        try {
+            List<IBoard> boards = firstOhBoardService.getBoardListTest();
+            boards.stream().map(board -> {
+                log.info("board Content non content: {}",board.toString());
+                return board;
+            }).collect(Collectors.toList());
+
+        }catch (Exception e){
+
+        }
+    }
+
+    @Test
+    public void 로그테스트(){
+        try {
+            log.debug("log debug Test Access {}","HelloWorld");
+            log.info("log info Test Access {}","HelloWorld");
+            throw new Exception();
+        }catch (Exception e){
+            log.error("error region : {}",e.toString());
+        }
     }
 
 }
